574db78c3f2210b479f21e41b6852da455c4e232
Marco Beier <marco-beier@t-online.de>
2020-03-15 20:48:25 +0100

    Did Step 5 of Navigation & Routing; Manual routing without changing the hash;

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,8 +1,20 @@
 sap.ui.define([
-  "com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
-  ], function(BaseController) {
-    "use strict";
-  
-    return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.Home", {});
-  });
-  
+	"com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.Home", {
+		onDisplayNotFound: function () {
+			/* 
+        alternative ways of getting the `sap.m.routing.Targets` Object
+			  this.getOwnerComponent().getRouter().getTargets() or 
+			  this.getOwnerComponent().getTargets().
+			*/
+			//display the "notFound" target without changing the hash
+			this.getRouter().getTargets().display("notFound", {
+        fromTarget: "home"
+      });
+
+		}
+	});
+});
--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,9 +1,28 @@
 sap.ui.define([
-    "com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
+	"com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
 ], function (BaseController) {
-    "use strict";
-    return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.NotFound", {
-        onInit: function () {
-        }
-    });
+	"use strict";
+	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.NotFound", {
+		onInit: function () {
+			var oRouter, oTarget;
+			oRouter = this.getRouter();
+			oTarget = oRouter.getTarget("notFound");
+			oTarget.attachDisplay(function (oEvent) {
+				this._oData = oEvent.getParameter("data"); // store the data
+			}, this);
+		},
+
+		// override the parent's onNavBack (inherited from BaseController)
+		onNavBack: function () {
+			// in some cases we could display a certain target when the back button is pressed
+			if (this._oData && this._oData.fromTarget) {
+				this.getRouter().getTargets().display(this._oData.fromTarget);
+				delete this._oData.fromTarget;
+				return;
+			}
+
+			// call the parent's onNavBack
+			BaseController.prototype.onNavBack.apply(this, arguments);
+		}
+	});
 });
