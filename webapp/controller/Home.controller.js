61af8c1732bf74596d5734dde805079aba068954
Marco Beier <marco-beier@t-online.de>
2020-03-21 16:45:31 +0100

    Step 6: Navigate to Routes with Hard-Coded Patterns

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -12,9 +12,11 @@ sap.ui.define([
 			*/
 			//display the "notFound" target without changing the hash
 			this.getRouter().getTargets().display("notFound", {
-        fromTarget: "home"
-      });
-
+				fromTarget: "home"
+			});
+		},
+		onNavToEmployees: function () {
+			this.getRouter().navTo("employeeList");
 		}
 	});
 });
---
+++ webapp/controller/employee/EmployeeList.controller.js
@@ -0,0 +1,6 @@
+sap.ui.define([
+	"com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.employee.EmployeeList", {});
+});
