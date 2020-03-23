f99257a0554b31470d75c0aa331b4f61154acc4b
Marco Beier <marco-beier@t-online.de>
2020-03-23 16:05:49 +0100

    Added everything for Step 7 of navigation tutorial; Navigating with mandatory parameters;

---
+++ webapp/controller/employee/Employee.controller.js
@@ -0,0 +1,48 @@
+sap.ui.define([
+	"com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
+], function (
+	BaseController
+) {
+	"use strict";
+
+	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.employee.Employee", {
+		onInit: function () {
+			var oRouter = this.getRouter();
+			oRouter.getRoute("employee").attachMatched(this._onRouteMatched, this);
+			// Hint: we don't want to do it this way
+			/*
+			oRouter.attachRouteMatched(function (oEvent){
+				var sRouteName, oArgs, oView;
+				sRouteName = oEvent.getParameter("name");
+				if (sRouteName === "employee"){
+					this._onRouteMatched(oEvent);
+				}
+			}, this);
+			*/
+		},
+		_onRouteMatched: function (oEvent) {
+			var oArgs, oView;
+			oArgs = oEvent.getParameter("arguments");
+			oView = this.getView();
+			//use oDataModel to resolve the path and attach events
+			oView.bindElement({
+				path: "/Employees(" + oArgs.employeeId + ")",
+				events: {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function (oEvent) {
+						oView.setBusy(true);
+					},
+					dataReceived: function (oEvent) {
+						oView.setBusy(false);
+					}
+				}
+			});
+		},
+		_onBindingChange: function (oEvent) {
+			// No data for the binding
+			if (!this.getView().getBindingContext()) {
+				this.getRouter().getTargets().display("notFound");
+			}
+		}
+	});
+});
--- webapp/controller/employee/EmployeeList.controller.js
+++ webapp/controller/employee/EmployeeList.controller.js
@@ -2,5 +2,14 @@ sap.ui.define([
 	"com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
 ], function (BaseController) {
 	"use strict";
-	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.employee.EmployeeList", {});
+	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.employee.EmployeeList", {
+		onListItemPressed: function (oEvent) {
+			var oItem, oCtx;
+			oItem = oEvent.getSource();
+			oCtx = oItem.getBindingContext();
+			this.getRouter().navTo("employee", {
+				employeeId: oCtx.getProperty("EmployeeID")
+			});
+		}
+	});
 });
