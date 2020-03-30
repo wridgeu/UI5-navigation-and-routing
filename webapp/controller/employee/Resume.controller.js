66a8048db3790aafb779202dd82fd7c9d1a312ce
Marco Beier <marco-beier@t-online.de>
2020-03-30 18:28:57 +0200

    Added anything needed for Step 9 of the Navigation Tutorial; Bookmarkable Tabs with Optional Query Parameters

--- webapp/controller/employee/Resume.controller.js
+++ webapp/controller/employee/Resume.controller.js
@@ -1,16 +1,21 @@
 sap.ui.define([
-	"com/mrb/UI5-Navigation-and-Routing/controller/BaseController"
+	"com/mrb/UI5-Navigation-and-Routing/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
 ], function (
-	BaseController
+	BaseController,
+	JSONModel
 ) {
 	"use strict";
+	var _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.employee.Resume", {
 		onInit: function () {
 			var oRouter = this.getRouter();
+			this.getView().setModel(new JSONModel(), "view");
 			oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
 		},
 		_onRouteMatched: function (oEvent) {
-			var oArgs, oView;
+			var oArgs, oView, oQuery;
+			//Read the passed in arguments
 			oArgs = oEvent.getParameter("arguments");
 			oView = this.getView();
 			oView.bindElement({
@@ -25,12 +30,35 @@ sap.ui.define([
 					}
 				}
 			});
+			oQuery = oArgs["?query"];
+			if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1) {
+				oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);
+			} else {
+				// the default query param should be visible at all time
+				this.getRouter().navTo("employeeResume", {
+					employeeId: oArgs.employeeId,
+					"?query": {
+						tab: _aValidTabKeys[0]
+					}
+				}, true /*no history*/ );
+			}
 		},
 		_onBindingChange: function (oEvent) {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
 			}
+		},
+		onTabSelect: function (oEvent) {
+			var oCtx = this.getView().getBindingContext();
+			//https://sapui5.hana.ondemand.com/#/api/sap.ui.core.routing.Router%23methods/navTo
+			//:?queryParameter: -> :: -> optional | {someParameter} -> {} ->  mandatory 
+			this.getRouter().navTo("employeeResume", {
+				employeeId: oCtx.getProperty("EmployeeID"),
+				"?query": {
+					tab: oEvent.getParameter("selectedKey")
+				}
+			}, true /*without history*/ );
 		}
 	});
 });
