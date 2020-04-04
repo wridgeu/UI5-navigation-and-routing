67954255fa08fc8d361a7c6302b40471ae1df2c6
Marco Beier <marco-beier@t-online.de>
2020-04-04 11:46:15 +0200

    Added everything for Step 11: Assign Multiple Targets

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -17,6 +17,9 @@ sap.ui.define([
 		},
 		onNavToEmployees: function () {
 			this.getRouter().navTo("employeeList");
+		},
+		onNavToEmployeeOverview : function ()  {
+			this.getRouter().navTo("employeeOverview");
 		}
 	});
 });
