9298a5d451a8ae5ea83c207c041c28bc513dcfeb
Marco Beier <marco-beier@t-online.de>
2020-04-10 17:09:44 +0200

    Added everything for Step 17: Listen to Matched Events of Any Route

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -21,6 +21,12 @@ sap.ui.define([
 				// telling what resource the user tried to access...
 				Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
 			});
+			oRouter.attachRouteMatched(function (oEvent){
+				var sRouteName = oEvent.getParameter("name");
+				// do something, i.e. send usage statistics to back end
+				// in order to improve our app and the user experience (Build-Measure-Learn cycle)
+				Log.info("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
+			});
 		}
 	});
 });
