3d1dc08ee3de51c919132c3fc17f91052e3ecbb7
Marco Beier <marco-beier@t-online.de>
2020-04-11 11:29:14 +0200

    enh: Implemented messagetoast in app root (app view); Reverted logLevel change for clean console

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
   "com/mrb/UI5-Navigation-and-Routing/controller/BaseController",
-  "sap/base/Log"
-], function (BaseController, Log) {
+  "sap/base/Log",
+  "sap/m/MessageToast"
+], function (BaseController, Log, MsgToast) {
 	"use strict";
 
 	return BaseController.extend("com.mrb.UI5-Navigation-and-Routing.controller.App", {
@@ -11,7 +12,7 @@ sap.ui.define([
 			// in order to see the debug info in the console, the log level needs to be explicitly
 			// set to INFO here.
 			// But for application development, the log level doesn't need to be set again in the code.
-			Log.setLevel(Log.Level.INFO);
+			//Log.setLevel(Log.Level.INFO);
 
 			var oRouter = this.getRouter();
 
@@ -19,13 +20,20 @@ sap.ui.define([
 				var sHash = oEvent.getParameter("hash");
 				// do something here, i.e. send logging data to the backend for analysis
 				// telling what resource the user tried to access...
-				Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+				//Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+				MsgToast.show("Invalid hash: '" + sHash + "'. Bypassed was hit.");
 			});
 			oRouter.attachRouteMatched(function (oEvent){
 				var sRouteName = oEvent.getParameter("name");
+				var sHash = oEvent.getSource().oHashChanger.hash;
 				// do something, i.e. send usage statistics to back end
 				// in order to improve our app and the user experience (Build-Measure-Learn cycle)
 				Log.info("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
+				if (sHash) {
+					MsgToast.show("User navigated to route '" + sRouteName + "'. " + "Hash: '" + sHash + "'.");
+				} else {
+					MsgToast.show("User navigated to route '" + sRouteName + "'.");
+				}
 			});
 		}
 	});
